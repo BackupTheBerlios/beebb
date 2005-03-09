64db76b5e5bf24f8140cb0bb5c29d432d5264bb3
pawelb <pawelb>
2005-03-09 12:04:30 +0000

    - zaslepka #1

--- Bee/program/src/pl/ltd/bee/Watek.java
+++ Bee/program/src/pl/ltd/bee/Watek.java
@@ -6,15 +6,30 @@
 
 package pl.ltd.bee;
 
+
+import java.util.*;
 /**
  *
  * @author pawelb
  */
 public class Watek {
     
-    /** Creates a new instance of Watek */
-    public Watek() {
-           
+    private long ID;
+    private int ID_Autor;
+    private String Temat;
+    private String Data;
+    
+    /** Creates a new instance of Watek
+     *@param ID - id watku
+     *@param ID_Autor - id autora watku
+     *@param Temat - temat watku
+     *@param Data - data watku
+     */
+    public Watek(long ID, int ID_Autor, String Temat, String Data)  {
+        this.ID=ID;
+        this.ID_Autor=ID_Autor;
+        this.Temat=Temat;
+        this.Data=Data;
     }
     
 }
