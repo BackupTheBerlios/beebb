60f7f1685370d167f3d0aaa1c0a7e42a4702b197
pawelb <pawelb>
2005-03-09 20:54:53 +0000

    - pare kosmetycznych zmian - mam nadzieje ze nic nie popsulem

--- Bee/program/src/pl/ltd/bee/ConnectorDB.java
+++ Bee/program/src/pl/ltd/bee/ConnectorDB.java
@@ -6,71 +6,55 @@ import java.lang.*;
 
 public class ConnectorDB {
     
-    private String HOST;
-    private String USER;
-    private String PASS;
+    private String Host;
+    private String User;
+    private String Pass;
     
-    public ConnectorDB() {}
-
-    public ConnectorDB(String Host, String User, String Pass)
-     {
-        this.HOST=Host;
-        this.USER=User;
-        this.PASS=Pass;
-     }
-    public void setParamaters(String Host, String User, String Pass)
-    {
-        this.HOST=Host;
-        this.USER=User;
-        this.PASS=Pass;
+    public ConnectorDB(String Host, String User, String Pass) {
+        setParameters(Host,User,Pass);
     }
-    /**Metoda zwraca liste wierszy ktore zwieraja hashtable: 
+    
+    public void setParameters(String Host, String User, String Pass) {
+        this.Host=Host;
+        this.User=User;
+        this.Pass=Pass;
+    }
+    
+    /**Metoda zwraca liste wierszy ktore zwieraja hashtable:
      * klucz:nazwa kolumny       wartosc: wartosc w kolumnie
      **/
-    public ArrayList query(String q)
-    {
-       ArrayList pom=new ArrayList();
-       Hashtable row;
-       Connection con=null;
-       int licz,i;
-     
-      try {
-        con = DriverManager.getConnection(HOST,USER,PASS);
-        Statement select = con.createStatement();
-        ResultSet result = select.executeQuery(q);
-        ResultSetMetaData rsmd = result.getMetaData();
-        licz= rsmd.getColumnCount();
-
-        while(result.next()) 
-         {
-           row=new Hashtable();         
-           for(i=0;i<licz;i++)
-            {
-             row.put(rsmd.getColumnName(i),result.getString(i));   
+    public ArrayList query(String q) {
+        ArrayList pom=new ArrayList();
+        try {
+            Connection con = DriverManager.getConnection(Host,User,Pass);
+            Statement select = con.createStatement();
+            ResultSet result = select.executeQuery(q);
+            ResultSetMetaData rsmd = result.getMetaData();
+            int licz = rsmd.getColumnCount();
+            
+            while(result.next()) {
+                Hashtable row=new Hashtable();
+                for(int i=0;i<licz;i++) {
+                    row.put(rsmd.getColumnName(i),result.getString(i));
+                }
+                pom.add(row);
             }
-           pom.add(row);
-         }
-         con.close();
-        }
-       catch( Exception e ) { e.printStackTrace(); }
-       
-       return pom;
-      }
-     
-    /**metoda wykonuje Insert, Update lub Delete **/
-    public boolean dmlQuery(String q)
-    {
-      Connection con=null;
-      int wynik=0;
-     try{
-         con = DriverManager.getConnection(HOST,USER,PASS);
-         Statement select = con.createStatement();
-         wynik = select.executeUpdate(q);
-      
-         con.close();   
-        }
-      catch( Exception e ) { e.printStackTrace(); }
-      if (wynik==0) return false;
-      else return true;
+            
+            con.close();
+        } catch( Exception e ) { e.printStackTrace(); }
+        return pom;
     }
+    
+    /**metoda wykonuje Insert, Update lub Delete **/
+    public boolean dmlQuery(String q) {
+        try{
+            Connection con = DriverManager.getConnection(Host,User,Pass);
+            Statement select = con.createStatement();
+            int wynik = select.executeUpdate(q);
+            con.close();
+            return (wynik!=0);
+        } catch( Exception e ) { e.printStackTrace(); }
+        return false;
+    }
+
 }
