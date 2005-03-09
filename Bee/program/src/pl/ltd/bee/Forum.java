a9456f03443d390508dae7677ecbdd2966cc292b
pawelb <pawelb>
2005-03-09 16:30:14 +0000

    - init

---
+++ Bee/program/src/pl/ltd/bee/Forum.java
@@ -0,0 +1,24 @@
+/*
+ * Forum.java
+ *
+ * Created on 9 marzec 2005, 17:27
+ */
+
+package pl.ltd.bee;
+
+import java.util.*;
+
+/**
+ *
+ * @author pawelb
+ */
+public class Forum {
+    
+    private String Nazwa;
+    
+    /** Creates a new instance of Forum */
+    public Forum(String Nazwa) {
+        this.Nazwa=Nazwa;
+    }
+    
+}
