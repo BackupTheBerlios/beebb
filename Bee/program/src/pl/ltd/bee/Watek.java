74af817af60cc559a455ce2632e178994ed1d585
pawelb <pawelb>
2005-03-09 11:22:38 +0000

    - init

---
+++ Bee/program/src/pl/ltd/bee/Watek.java
@@ -0,0 +1,20 @@
+/*
+ * Watek.java
+ *
+ * Created on 9 marzec 2005, 12:21
+ */
+
+package pl.ltd.bee;
+
+/**
+ *
+ * @author pawelb
+ */
+public class Watek {
+    
+    /** Creates a new instance of Watek */
+    public Watek() {
+           
+    }
+    
+}
