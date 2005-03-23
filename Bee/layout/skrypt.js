006a2fe8583c2d08d3841c3933fd2f7439693d79
wilk <wilk>
2005-03-23 13:05:34 +0000

    - no to jeszcze lepszy JS

--- Bee/layout/skrypt.js
+++ Bee/layout/skrypt.js
@@ -8,4 +8,20 @@ function resizeMain()
 			top.document.getElementById('frameHead').height = top.frames[0].document.getElementById('tableHeader').offsetHeight +30;
 			top.document.getElementById('cellHead').height = top.frames[0].document.getElementById('tableHeader').offsetHeight +40;
 		}
+		
+		//poprawianie dolnej ramki
+		if (top.frames[1].location.pathname.indexOf("forum.html",0) != -1) //oczywiscie w JSP bedziemy szukac czegos na wzor show_forum= , a w tej chwili jest to np kid=
+		if (top.frames[1].document.getElementById('tableForum')) //zabezpieczenie by nie wyszlo zero		
+		{
+			top.document.getElementById('frameTresc').height = top.frames[1].document.getElementById('tableForum').offsetHeight +30;
+			top.document.getElementById('cellTresc').height = top.frames[1].document.getElementById('tableForum').offsetHeight +40;
+		}
+		
+		if (top.frames[1].location.pathname.indexOf("podforum.html",0) != -1) 
+		if (top.frames[1].document.getElementById('tablePodforum')) //zabezpieczenie by nie wyszlo zero		
+		{
+			top.document.getElementById('frameTresc').height = top.frames[1].document.getElementById('tablePodforum').offsetHeight +30;
+			top.document.getElementById('cellTresc').height = top.frames[1].document.getElementById('tablePodforum').offsetHeight +40;
+		}
+		
 }
