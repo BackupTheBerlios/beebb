3d050d740add7bff4e47d9212dc77f73ed9cb669
wilk <wilk>
2005-03-24 20:50:52 +0000

    - wyjatki

---
+++ Bee/program/src/pl/ltd/bee/Exceptions/BeeException.java
@@ -0,0 +1,22 @@
+/*
+ * BeeException.java
+ *
+ * Created on 24 marzec 2005, 18:04
+ */
+
+package pl.ltd.bee.Exceptions;
+
+/**
+ *
+ * @author pawelb
+ */
+public class BeeException extends Exception{
+    
+    /** Tworzy nowa instancje BeeException 
+     * @param wyjatek tresc wyjatku reprezentowana jako string  
+     */
+    public BeeException(String wyjatek) {
+        super(wyjatek);
+    }
+    
+}
