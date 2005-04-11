1fdb91a17d182c1d266e932ed0d8fd5ed2ac8652
wilk <wilk>
2005-04-11 12:50:32 +0000

    - nowe tabele

--- Bee/baza_danych/Bee_MySQL.sql
+++ Bee/baza_danych/Bee_MySQL.sql
@@ -211,6 +211,56 @@ CREATE TABLE `Bee_New_User` (
 ) TYPE=InnoDB CHARACTER SET utf8 COMMENT='Tabela z kluczami do odblokowania konta nowego uzytkownika';
 
 
+--
+-- Tabela z grupami
+--
+
+DROP TABLE IF EXISTS `Bee_Groups`;
+CREATE TABLE `Bee_Groups` (
+  `ID` int(10) unsigned NOT NULL auto_increment,
+  `Nazwa` varchar(100) NOT NULL default '',
+  PRIMARY KEY (`ID`)
+) TYPE=InnoDB CHARACTER SET utf8 COMMENT='Tabela z dostepnymi grupami';
+
+--
+-- Tabela z grupami
+--
+
+DROP TABLE IF EXISTS `Bee_Users_Groups`;
+CREATE TABLE `Bee_Users_Groups` (
+  `ID_User` int(10) unsigned NOT NULL default '0',
+  `ID_Group` int(10) unsigned NOT NULL default '0',
+  KEY `id_u` (`ID_User`),
+  KEY `id_g` (`ID_Group`),
+  CONSTRAINT `Bee_Users_Groups_ibfk_1` FOREIGN KEY (`ID_User`) REFERENCES `Bee_Users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Bee_Users_Groups_ibfk_2` FOREIGN KEY (`ID_Group`) REFERENCES `Bee_Groups` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) TYPE=InnoDB CHARACTER SET utf8 COMMENT='Tabela z czlonkami grup';
+
+--
+-- Tabela z grupami
+--
+
+DROP TABLE IF EXISTS `Bee_Privilages`;
+CREATE TABLE `Bee_Privilages` (
+  `ID_Group` int(10) unsigned NOT NULL,
+  `ID_Kategoria` int(10) unsigned NULL,
+  `ID_Podforum` int(10) unsigned NULL,
+  `ID_Watek` int(10) unsigned NULL,
+  `Czytanie` enum('T','N') NOT NULL,
+  `Pisanie` enum('T','N') NOT NULL,
+  KEY `id_g` (`ID_Group`),
+  KEY `id_k` (`ID_Kategoria`),
+  KEY `id_p` (`ID_Podforum`),
+  KEY `id_w` (`ID_Watek`),
+  CONSTRAINT `Bee_Privilages_ibfk_1` FOREIGN KEY (`ID_Group`) REFERENCES `Bee_Groups` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Bee_Privilages_ibfk_2` FOREIGN KEY (`ID_Kategoria`) REFERENCES `Bee_Kategorie` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Bee_Privilages_ibfk_3` FOREIGN KEY (`ID_Podforum`) REFERENCES `Bee_Podfora` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Bee_Privilages_ibfk_4` FOREIGN KEY (`ID_Watek`) REFERENCES `Bee_Watki` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CHECK ((ID_Kategoria is not NULL) AND (ID_PODFORUM is NULL) AND (ID_Watek is NULL))
+) TYPE=InnoDB CHARACTER SET utf8 COMMENT='Tabela z prawami grup';
+
+
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
