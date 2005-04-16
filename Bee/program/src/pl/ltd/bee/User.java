19ffca2b0ae01263d1d1ad426a4082145ff06ac0
ps230665 <ps230665>
2005-04-16 16:32:37 +0000

    Teraz tylko User

--- Bee/program/src/pl/ltd/bee/User.java
+++ Bee/program/src/pl/ltd/bee/User.java
@@ -1,86 +1,160 @@
 /*
- * User.java
+ * RegisteredUser.java
  *
- * Created on 9 marzec 2005, 18:01
+ * Created on 18 marzec 2005, 22:18
  */
 
 package pl.ltd.bee;
 
 /**
  *
- * @author pawelb
+ * @author pawelb i sopi
  */
-public abstract class User {
+public class User {
+    private int ID;
+    private String login;
+    private String haslo;
+    private String imie;
+    private String nazwisko;
+    private String email;
+    private String gg;
+    private String jabber;
+    private String lastlog;
+    private boolean aktywny;
+    private boolean admin;
+    private boolean moderator;
+  
     
-    
-    /** Abstrakcyjny konstruktor abstrakcyjnej klasy */
-    public User() {};
-    
-    
-    /** Metoda sprawdza haslo
-     * @param passwd haslo w plain tekst
-     * @return ponieważ jest to abstrakcyjna klasa zwraca zawsze false
+    /** Tworzy obiekt RegisteredUser
+     * @param ID identyfikator uzytkownika
+     * @param haslo zakodowane haslo uzytkownika
+     * @pram imie imie uzytkownika
+     * @pram nazwisko nazwisko uzytkownika
+     * @param email email uzytkownika
+     * @param gg numer gadu-gadu
+     * @param jabber adres jabbera
+     * @param lastlog ostatnie logowanie
+     * @param aktywny czy użytkownik jest aktywny DataBase.TAK lub DataBase.NIE
      */
-    public abstract  boolean checkPasswd(String passwd);
+    public User(int ID, String login, String haslo, String imie, String nazwisko, String email, String gg, String jabber,String lastlog,String aktywny,String admin,String moderator) {
+        this.ID=ID;
+        this.login=login;
+        this.haslo=haslo;
+        this.imie=imie;
+        this.nazwisko=nazwisko;
+        this.email=email;
+        this.gg=gg;
+        this.jabber=jabber;
+        this.lastlog=lastlog;
+        if (aktywny.compareTo(DataBase.TAK)==0)
+            this.aktywny=true; else this.aktywny=false;
+        if (admin.compareTo(DataBase.TAK)==0)
+            this.admin=true; else this.admin=false;
+        if (moderator.compareTo(DataBase.TAK)==0)
+            this.moderator=true; else this.moderator=false;
+    }
     
     
-    /** Metoda sprawdza czy uzytkownik jest aktywny
-     * @return ponieważ jest to abstrakcyjna klasa zwraca zawsze false
+    /** Sprawdza czy podane hasło (plain) zgadza się
+     * @param passwd haslo w plain txt
+     * @return T lub F w zaleznosci czy haslo sie zgadza
      */
-    public abstract boolean aktywny();
+    public boolean checkPasswd(String passwd) {
+        return Crypto.matches(haslo, passwd);
+    }
+    
+    
+    /** metoda sprawdza czy uzytkownik jest aktywny
+     * @return T lub N w zaleznosci czy user jest aktywny czy nie
+     */
+    public boolean aktywny() {
+        return aktywny;
+    }
+    
+    /** metoda sprawdza czy uzytkownik jest administratorem
+     * @return T lub N w zaleznosci czy user jest administratorem czy nie
+     */
+    public boolean admin() {
+        return admin;
+    }
+    
+    /** metoda sprawdza czy uzytkownik jest moderatorem
+     * @return T lub N w zaleznosci czy user jest moderatorem czy nie
+     */
+    public boolean moderator() {
+        return moderator;
+    }
     
     
     /** Metoda zwraca ID użytkownika
      * @return Integer reprezentujacy uzytkownika
      */
-    public abstract int getID();
+    public int getID() {
+        return ID;
+    }
     
     
     /** Metoda zwraca zahashowane haslo uzytkownika
      * @return String z reprezentacja hasla
      **/
-    public abstract String getHaslo();
+    public String getHaslo(){
+        return haslo;
+    }
     
     
     /** Metoda zwraca login uzytkownika
      * @return String z nazwa Login
      **/
-    public abstract String getLogin();
+    public String getLogin(){
+        return this.login;
+    }
     
     
     /** Metoda zwraca e-mail uzytkownika
      * @return String z e-mailem
      **/
-    public abstract String getEmail();
+    public String getEmail(){
+        return this.email;
+    }
     
     
     /** Metoda zwraca imie uzytkownika
      * @return String z imieniem
      **/
-    public abstract String getImie();
+    public String getImie(){
+        return this.imie;
+    }
     
     
     /** Metoda zwraca nazwisko uzytkownika
      * @return String z nazwiskiem
      **/
-    public abstract String getNazwisko();
+    public String getNazwisko(){
+        return this.nazwisko;
+    }
     
     
     /** Metoda zwraca numer GG uzytkownika
      * @return String z numerem GG
      **/
-    public abstract String getGG();
+    public String getGG(){
+        return this.gg;
+    }
     
     
     /** Metoda zwraca konto jabber uzytkownika
      * @return String z kontem jabber
      **/
-    public abstract String getJabber();
+    public String getJabber(){
+        return this.jabber;
+    }
     
     
     /** Metoda zwraca date ostatniego zalogowania uzytkownika
      * @return String z datą ostatniego zalogowania
      **/
-    public abstract String getLastLog();
+    public String getLastLog(){
+        return this.lastlog;
+    }
     
 }
